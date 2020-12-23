3d563a134ed760290fbd5fddd91e94e13dafd7cf
sunilRF9 <sunil1996@gmail.com>
2020-12-23 10:59:05 +0530

    changed Debug

--- blog/settings.py
+++ blog/settings.py
@@ -68,6 +68,13 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
 
 DATABASES = {
     'default': {
--- blog/urls.py
+++ blog/urls.py
@@ -8,5 +8,4 @@ urlpatterns = [
     path('', include('posts.urls')),
     path('tinymce/', include('tinymce.urls')),
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
